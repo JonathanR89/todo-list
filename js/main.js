0d463c49fb4b3b4f7c337100f9512ce7508c0251
Jonathan <jonathanrautenbach@gmail.com>
2017-08-01 11:42:48 +0200

    edit todos

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+var TodoComponent = Vue.extend({
+	data: function(){
+		return{
+			inEditMode: false
+		}
+	},
+	props: ['todo'],
+	template: '<div><span v-on:click="clicked" v-show="!inEditMode">{{ todo.title }}</span><input v-on:keyup.enter="" v-model="todo.title" v-show="inEditMode"/></div>',
+	methods: {
+		clicked: function(){
+			this.inEditMode = true;
+		},
+		saved: function(){
+			this.inEditMode = false;
+		}
+	}
+});
+
+Vue.component('todo-component', TodoComponent);
+
+
 new Vue({
 	el: '#app',
 	data: {
@@ -27,6 +48,9 @@ new Vue({
 		},
 		increaseCounter: function () {
 			this.count = this.count+1;
+		},
+		removeTodo: function (index) {
+			this.todos.splice(index, 1);
 		}
 	}
 });	
